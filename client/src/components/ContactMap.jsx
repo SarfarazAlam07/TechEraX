b5405f4841e17ea246f6127d8b5f72ca3b73c9cc
sahil <161115424+sahilt56@users.noreply.github.com>
2026-01-27 16:18:48 +0530

    map section fix
    map section fix

--- client/src/components/ContactMap.jsx
+++ client/src/components/ContactMap.jsx
@@ -4,7 +4,6 @@ const ContactMap = () => {
   return (
     <section className="py-12 px-6 md:px-12 bg-white">
       <div className="max-w-7xl mx-auto rounded-3xl overflow-hidden shadow-lg border border-gray-100 h-[400px] relative bg-slate-100">
-        {/* Google Map Embed (Placeholder) */}
         <iframe
           title="Office Location"
           width="100%"
@@ -12,8 +11,9 @@ const ContactMap = () => {
           style={{ border: 0 }}
           loading="lazy"
           allowFullScreen
-          // Example: San Francisco Map
-          src="https://maps.google.com/maps?q=25.28,84.87+(TechEraX)&t=&z=14&ie=UTF8&iwloc=B&output=embed"
+          referrerPolicy="no-referrer-when-downgrade"
+          // Our Address
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14407.77252670731!2d84.8365287!3d25.4664468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x398d509c2a685955%3A0x26421d0141662998!2sPaliganj%2C%20Bihar!5e0!3m2!1sen!2sin!4v1706429944116!5m2!1sen!2sin"
         ></iframe>
       </div>
     </section>
