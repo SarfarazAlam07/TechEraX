6eed246cd37945adb99cdcfcf8e7f7d57a9202db
sahil <161115424+sahilt56@users.noreply.github.com>
2026-01-27 16:23:39 +0530

    again fix

--- client/src/components/ContactMap.jsx
+++ client/src/components/ContactMap.jsx
@@ -12,8 +12,9 @@ const ContactMap = () => {
           loading="lazy"
           allowFullScreen
           referrerPolicy="no-referrer-when-downgrade"
-          // Our Address
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14407.77252670731!2d84.8365287!3d25.4664468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x398d509c2a685955%3A0x26421d0141662998!2sPaliganj%2C%20Bihar!5e0!3m2!1sen!2sin!4v1706429944116!5m2!1sen!2sin"
+          // FIX: Maine yahan standard Google Maps Embed link (HTTPS) use kiya hai
+          // Location: Paliganj, Patna
+          src="https://maps.google.com/maps?q=Paliganj,%20Patna&t=&z=13&ie=UTF8&iwloc=&output=embed"
         ></iframe>
       </div>
     </section>
