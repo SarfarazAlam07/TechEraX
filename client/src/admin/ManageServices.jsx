2d56d5c086d225b5d93ee6db31b1375f10132d4e
Sarfaraz Alam <sarfaraz7277alam@gmail.com>
2026-01-29 23:14:47 +0530

    solve bug

--- client/src/admin/ManageServices.jsx
+++ client/src/admin/ManageServices.jsx
@@ -6,7 +6,7 @@ import {
 import { useData } from "../context/DataContext";
 import axios from "axios"; 
 // ✅ Import Modal
-import ConfirmationModal from "./ConfirmationModal";
+import ConfirmationModal from "../components/ConfirmationModal";
 
 const ManageServices = () => {
   const { services, refreshData, API_URL } = useData();
