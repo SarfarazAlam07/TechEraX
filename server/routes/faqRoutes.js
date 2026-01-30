b6d571b893bcd183076014611cb6f4f23cc8b7ea
sahil <161115424+sahilt56@users.noreply.github.com>
2026-01-30 17:50:24 +0530

    Update faqRoutes.js

--- server/routes/faqRoutes.js
+++ server/routes/faqRoutes.js
@@ -1,9 +1,20 @@
 import express from "express";
-import { getFaqs, createFaq, updateFaq, deleteFaq } from "../controllers/faqController.js";
+import { 
+  getFaqs, 
+  createFaq, 
+  updateFaq, 
+  deleteFaq,
+  updateFaqOrder // ✅ Import New Controller
+} from "../controllers/faqController.js";
+
 const router = express.Router();
 
 router.get("/", getFaqs);
 router.post("/", createFaq);
+
+// ✅ REORDER ROUTE (Isse /:id se PEHLE rakhna zaroori hai)
+router.put("/reorder", updateFaqOrder);
+
 router.put("/:id", updateFaq);
 router.delete("/:id", deleteFaq);
 
