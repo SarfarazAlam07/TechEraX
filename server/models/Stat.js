b70db4fac7f6526a0e8d3abc308d612497d8579c
sahil <161115424+sahilt56@users.noreply.github.com>
2026-01-30 17:43:54 +0530

    Update Stat.js

--- server/models/Stat.js
+++ server/models/Stat.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
 const statSchema = new mongoose.Schema({
-  label: { type: String, required: true }, // e.g., "Projects Delivered"
-  value: { type: String, required: true }  // e.g., "100+"
+  label: { type: String, required: true },
+  value: { type: String, required: true },
+  // ✅ Added Order Field
+  order: { type: Number, default: 100 }
 }, { timestamps: true });
 
 export default mongoose.model("Stat", statSchema);
